91e0548b3b5f08122d590efca0c1b0bd8c9b56fa
Mary-Rose Tracy <maryrose.tracy@maverick>
2023-04-18 02:35:50 -0500

    DCUST

---
+++ P10/store/Store.java
@@ -0,0 +1,94 @@
+/*
+Name: Mary-Rose Tracy 
+ID#:1001852753 
+Assignment: P06 Email
+TODO: see program comments below
+we ain't gonna do touch this file essentially
+credit to prof rice
+*/
+package store;
+
+import java.util.ArrayList;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+
+public class Store 
+{
+    public Store(String name) 
+    {
+        this.name = name;
+    }
+     public Store(BufferedReader br) throws IOException {
+        this.name = br.readLine();
+        int numOptions = Integer.parseInt(br.readLine());
+        while(numOptions-- > 0)
+            customers.add(new Customer(br));
+
+        numOptions = Integer.parseInt(br.readLine());
+        while(numOptions-- > 0)
+            options.add(new Option(br));
+
+        numOptions = Integer.parseInt(br.readLine());
+        while(numOptions-- > 0)
+            computers.add(new Computer(br));
+
+        numOptions = Integer.parseInt(br.readLine());
+        while(numOptions-- > 0)
+            orders.add(new Order(br));
+    }
+    public void save(BufferedWriter bw) throws IOException {
+        bw.write(name + '\n');
+        
+        bw.write("" + customers.size() + '\n');
+        for(Customer customer : customers)
+            customer.save(bw);
+
+        bw.write("" + options.size() + '\n');
+        for(Option option : options)
+            option.save(bw);
+
+        bw.write("" + computers.size() + '\n');
+        for(Computer computer : computers)
+            computer.save(bw);
+
+        bw.write("" + orders.size() + '\n');
+        for(Order order : orders)
+            order.save(bw);
+
+    }
+   public String name() {
+        return this.name;
+    }
+    public void add(Customer customer) {
+        if(!customers.contains(customer)) customers.add(customer);
+    }
+    public Object[] customers() {
+        return this.customers.toArray();
+    }
+    public void add(Option option) {
+        if(!options.contains(option)) options.add(option);
+    }
+    public Object[] options() {
+        return this.options.toArray();
+    }
+    public void add(Computer computer) {
+        if(!computers.contains(computer)) computers.add(computer);
+    }
+    public Object[] computers() {
+        return this.computers.toArray();
+    }
+    public void add(Order order) {
+        if(!orders.contains(order)) orders.add(order);
+    }
+    public Object[] orders() {
+        return this.orders.toArray();
+    }
+    private String name;
+    private ArrayList<Customer> customers = new ArrayList<>();
+    private ArrayList<Option> options = new ArrayList<>();
+    private ArrayList<Computer> computers = new ArrayList<>();
+    private ArrayList<Order> orders = new ArrayList<>();
+}
+
